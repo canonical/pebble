f345a8323f0c61e4dcb8f2fdc0dbf1894b24002c
Ben Hoyt <ben.hoyt@canonical.com>
2021-11-25 17:52:44 +1300

    Test for on-check-failure logic

--- internal/overlord/servstate/manager_test.go
+++ internal/overlord/servstate/manager_test.go
@@ -33,6 +33,7 @@ import (
 	"gopkg.in/yaml.v3"
 
 	"github.com/canonical/pebble/internal/logger"
+	"github.com/canonical/pebble/internal/overlord/checkstate"
 	"github.com/canonical/pebble/internal/overlord/servstate"
 	"github.com/canonical/pebble/internal/overlord/state"
 	"github.com/canonical/pebble/internal/plan"
@@ -751,7 +752,7 @@ func (f writerFunc) Write(p []byte) (int, error) {
 	return f(p)
 }
 
-func (s *S) TestRestart(c *C) {
+func (s *S) TestActionRestart(c *C) {
 	// Add custom backoff delay so it auto-restarts quickly.
 	layer := parseLayer(c, 0, "layer", `
 services:
@@ -860,6 +861,302 @@ services:
 	})
 }
 
+func (s *S) TestOnCheckFailureRestartWhileRunning(c *C) {
+	// Create check manager and tell it about plan updates
+	checkMgr := checkstate.NewManager()
+	s.manager.AddPlanHandler(checkMgr.Configure)
+
+	// Tell service manager about check failures
+	checkMgr.AddFailureHandler(s.manager.CheckFailure)
+
+	tempDir := c.MkDir()
+	tempFile := filepath.Join(tempDir, "out")
+	layer := parseLayer(c, 0, "layer", fmt.Sprintf(`
+services:
+    test2:
+        override: replace
+        command: /bin/sh -c 'echo x >>%s; sleep 1'
+        on-check-failure:
+            chk1: restart
+
+checks:
+    chk1:
+         override: replace
+         period: 75ms  # a bit longer than shortOkayWait
+         failures: 1
+         exec:
+             command: will-fail
+`, tempFile))
+	err := s.manager.AppendLayer(layer)
+	c.Assert(err, IsNil)
+
+	// Start service and wait till it starts up (output file is written to)
+	s.startServices(c, []string{"test2"}, 1)
+	for i := 0; ; i++ {
+		if i >= 100 {
+			c.Fatalf("failed waiting for command to start")
+		}
+		b, _ := ioutil.ReadFile(tempFile)
+		if string(b) == "x\n" {
+			break
+		}
+		time.Sleep(5 * time.Millisecond)
+	}
+
+	// Now wait till check happens (it will-fail)
+	for i := 0; ; i++ {
+		if i >= 100 {
+			c.Fatalf("failed waiting for check to fail")
+		}
+		checks, err := checkMgr.Checks("", nil)
+		c.Assert(err, IsNil)
+		if len(checks) == 1 && !checks[0].Healthy {
+			c.Assert(checks[0].Failures, Equals, 1)
+			c.Assert(checks[0].LastError, Matches, ".* executable file not found .*")
+			break
+		}
+		time.Sleep(5 * time.Millisecond)
+	}
+
+	// Check failure should terminate process and restart it, so wait for that
+	for i := 0; ; i++ {
+		if i >= 100 {
+			c.Fatalf("failed waiting for command to start")
+		}
+		b, err := ioutil.ReadFile(tempFile)
+		c.Assert(err, IsNil)
+		if string(b) == "x\nx\n" {
+			break
+		}
+		time.Sleep(5 * time.Millisecond)
+	}
+
+	// Shouldn't be restarted again
+	time.Sleep(100 * time.Millisecond)
+	b, err := ioutil.ReadFile(tempFile)
+	c.Assert(err, IsNil)
+	c.Assert(string(b), Equals, "x\nx\n")
+	checks, err := checkMgr.Checks("", nil)
+	c.Assert(err, IsNil)
+	c.Assert(len(checks), Equals, 1)
+	c.Assert(checks[0].Healthy, Equals, false)
+	c.Assert(checks[0].LastError, Matches, ".* executable file not found .*")
+	svc := s.serviceByName(c, "test2")
+	c.Assert(svc.Current, Equals, servstate.StatusActive)
+}
+
+func (s *S) TestOnCheckFailureRestartDuringBackoff(c *C) {
+	// Create check manager and tell it about plan updates
+	checkMgr := checkstate.NewManager()
+	s.manager.AddPlanHandler(checkMgr.Configure)
+
+	// Tell service manager about check failures
+	checkMgr.AddFailureHandler(s.manager.CheckFailure)
+
+	tempDir := c.MkDir()
+	tempFile := filepath.Join(tempDir, "out")
+	layer := parseLayer(c, 0, "layer", fmt.Sprintf(`
+services:
+    test2:
+        override: replace
+        command: /bin/sh -c 'echo x >>%s; sleep 0.075'
+        backoff-factor: 10  # ensure it only backoff-restarts once
+        on-check-failure:
+            chk1: restart
+
+checks:
+    chk1:
+         override: replace
+         period: 100ms
+         failures: 1
+         exec:
+             command: will-fail
+`, tempFile))
+	err := s.manager.AppendLayer(layer)
+	c.Assert(err, IsNil)
+
+	// Start service and wait till it starts up (output file is written to)
+	s.startServices(c, []string{"test2"}, 1)
+	for i := 0; ; i++ {
+		if i >= 100 {
+			c.Fatalf("failed waiting for command to start")
+		}
+		b, _ := ioutil.ReadFile(tempFile)
+		if string(b) == "x\n" {
+			break
+		}
+		time.Sleep(5 * time.Millisecond)
+	}
+
+	// Ensure it exits and goes into backoff state
+	s.waitUntilService(c, "test2", func(svc *servstate.ServiceInfo) bool {
+		return svc.Current == servstate.StatusBackoff
+	})
+
+	// Check failure should start it again, so wait for that
+	for i := 0; ; i++ {
+		if i >= 100 {
+			c.Fatalf("failed waiting for command to start")
+		}
+		b, err := ioutil.ReadFile(tempFile)
+		c.Assert(err, IsNil)
+		if string(b) == "x\nx\n" {
+			break
+		}
+		time.Sleep(5 * time.Millisecond)
+	}
+	svc := s.serviceByName(c, "test2")
+	c.Assert(svc.Current, Equals, servstate.StatusActive)
+	c.Assert(s.manager.BackoffNum("test2"), Equals, 0)
+
+	// Shouldn't be restarted again
+	time.Sleep(125 * time.Millisecond)
+	b, err := ioutil.ReadFile(tempFile)
+	c.Assert(err, IsNil)
+	c.Assert(string(b), Equals, "x\nx\n")
+	checks, err := checkMgr.Checks("", nil)
+	c.Assert(err, IsNil)
+	c.Assert(len(checks), Equals, 1)
+	c.Assert(checks[0].Healthy, Equals, false)
+	c.Assert(checks[0].LastError, Matches, ".* executable file not found .*")
+}
+
+func (s *S) TestOnCheckFailureIgnore(c *C) {
+	// Create check manager and tell it about plan updates
+	checkMgr := checkstate.NewManager()
+	s.manager.AddPlanHandler(checkMgr.Configure)
+
+	// Tell service manager about check failures
+	checkMgr.AddFailureHandler(s.manager.CheckFailure)
+
+	tempDir := c.MkDir()
+	tempFile := filepath.Join(tempDir, "out")
+	layer := parseLayer(c, 0, "layer", fmt.Sprintf(`
+services:
+    test2:
+        override: replace
+        command: /bin/sh -c 'echo x >>%s; sleep 1'
+        on-check-failure:
+            chk1: ignore
+
+checks:
+    chk1:
+         override: replace
+         period: 75ms  # a bit longer than shortOkayWait
+         failures: 1
+         exec:
+             command: will-fail
+`, tempFile))
+	err := s.manager.AppendLayer(layer)
+	c.Assert(err, IsNil)
+
+	// Start service and wait till it starts up (output file is written to)
+	s.startServices(c, []string{"test2"}, 1)
+	for i := 0; ; i++ {
+		if i >= 100 {
+			c.Fatalf("failed waiting for command to start")
+		}
+		b, _ := ioutil.ReadFile(tempFile)
+		if string(b) == "x\n" {
+			break
+		}
+		time.Sleep(5 * time.Millisecond)
+	}
+
+	// Now wait till check happens (it will-fail)
+	for i := 0; ; i++ {
+		if i >= 100 {
+			c.Fatalf("failed waiting for check to fail")
+		}
+		checks, err := checkMgr.Checks("", nil)
+		c.Assert(err, IsNil)
+		if len(checks) == 1 && !checks[0].Healthy {
+			c.Assert(checks[0].Failures, Equals, 1)
+			c.Assert(checks[0].LastError, Matches, ".* executable file not found .*")
+			break
+		}
+		time.Sleep(5 * time.Millisecond)
+	}
+
+	// Service shouldn't have been restarted
+	time.Sleep(100 * time.Millisecond)
+	b, err := ioutil.ReadFile(tempFile)
+	c.Assert(err, IsNil)
+	c.Assert(string(b), Equals, "x\n")
+	checks, err := checkMgr.Checks("", nil)
+	c.Assert(err, IsNil)
+	c.Assert(len(checks), Equals, 1)
+	c.Assert(checks[0].Healthy, Equals, false)
+	c.Assert(checks[0].LastError, Matches, ".* executable file not found .*")
+	svc := s.serviceByName(c, "test2")
+	c.Assert(svc.Current, Equals, servstate.StatusActive)
+}
+
+func (s *S) TestOnCheckFailureHalt(c *C) {
+	// Create check manager and tell it about plan updates
+	checkMgr := checkstate.NewManager()
+	s.manager.AddPlanHandler(checkMgr.Configure)
+
+	// Tell service manager about check failures
+	checkMgr.AddFailureHandler(s.manager.CheckFailure)
+
+	tempDir := c.MkDir()
+	tempFile := filepath.Join(tempDir, "out")
+	layer := parseLayer(c, 0, "layer", fmt.Sprintf(`
+services:
+    test2:
+        override: replace
+        command: /bin/sh -c 'echo x >>%s; sleep 1'
+        on-check-failure:
+            chk1: halt
+
+checks:
+    chk1:
+         override: replace
+         period: 75ms  # a bit longer than shortOkayWait
+         failures: 1
+         exec:
+             command: will-fail
+`, tempFile))
+	err := s.manager.AppendLayer(layer)
+	c.Assert(err, IsNil)
+
+	// Start service and wait till it starts up (output file is written to)
+	s.startServices(c, []string{"test2"}, 1)
+	for i := 0; ; i++ {
+		if i >= 100 {
+			c.Fatalf("failed waiting for command to start")
+		}
+		b, _ := ioutil.ReadFile(tempFile)
+		if string(b) == "x\n" {
+			break
+		}
+		time.Sleep(5 * time.Millisecond)
+	}
+
+	// Now wait till check happens (it will-fail)
+	for i := 0; ; i++ {
+		if i >= 100 {
+			c.Fatalf("failed waiting for check to fail")
+		}
+		checks, err := checkMgr.Checks("", nil)
+		c.Assert(err, IsNil)
+		if len(checks) == 1 && !checks[0].Healthy {
+			c.Assert(checks[0].Failures, Equals, 1)
+			c.Assert(checks[0].LastError, Matches, ".* executable file not found .*")
+			break
+		}
+		time.Sleep(5 * time.Millisecond)
+	}
+
+	// It should have closed the stopDaemon channel.
+	select {
+	case <-s.stopDaemon:
+	case <-time.After(time.Second):
+		c.Fatalf("timed out waiting for stop-daemon channel")
+	}
+}
+
 func (s *S) waitUntilService(c *C, service string, f func(svc *servstate.ServiceInfo) bool) *servstate.ServiceInfo {
 	for i := 0; i < 20; i++ {
 		svc := s.serviceByName(c, service)
@@ -868,11 +1165,11 @@ func (s *S) waitUntilService(c *C, service string, f func(svc *servstate.Service
 		}
 		time.Sleep(10 * time.Millisecond)
 	}
-	c.Fatalf("failed waiting for service")
+	c.Fatalf("timed out waiting for service")
 	return nil // satisfy compiler
 }
 
-func (s *S) TestExit(c *C) {
+func (s *S) TestActionHalt(c *C) {
 	layer := parseLayer(c, 0, "layer", `
 services:
     test2:
