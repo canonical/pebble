9090242db9d08a4964318bbffd420854fd928895
Robert Carlsen <robert.carlsen@canonical.com>
2022-06-24 11:38:05 -0600

    sorting out tls transport of messages

--- internal/servicelog/formatter.go
+++ internal/servicelog/formatter.go
@@ -15,20 +15,60 @@
 package servicelog
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
 	"io"
+	"net"
 	"sync"
 	"time"
 )
 
-type syslogTCP struct {
-	Host string
+type SyslogWriter struct {
+	conn   net.Conn
+	frame  func([]byte) []byte
+	format func([]byte) []byte
 }
 
-func (s *syslogTCP) Write(p []byte) (int, error) {
-	panic("unimplemented")
+func NewSyslogWriter(host string, clientCert tls.Certificate, serverCert []byte) (*SyslogWriter, error) {
+	// TODO: figure out how all the cert stuff actually works
+	var pool *x509.CertPool
+	if serverCert != nil {
+		pool = x509.NewCertPool()
+		pool.AppendCertsFromPEM(serverCert)
+	}
+	config := tls.Config{RootCAs: pool, Certificates: []tls.Certificate{clientCert}}
+
+	conn, err := tls.Dial("tcp", host, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	frame := func(p []byte) []byte { return []byte(fmt.Sprintf("%d %s", len(p), p)) }
+	format := func(content []byte) []byte {
+		timestamp := time.Now().Format(time.RFC3339)
+		pid := 42         // TODO: Make this be service PID
+		app := "foo"      // TODO: make this be service name
+		hostname := "bar" // TODO: make this be workload container host
+		tag := "TODO_something"
+		priority := 14 // NOTE: see RFC 5424 6.2.1 for available codes
+		version := 1
+		msg := fmt.Sprintf("<%d>%d %s %s %s %d %s - %s",
+			priority, version, timestamp, hostname, app, pid, tag, content)
+		return []byte(msg)
+	}
+
+	return &SyslogWriter{conn: conn, frame: frame, format: format}, nil
+}
+
+func (s *SyslogWriter) Close() error { return s.conn.Close() }
+
+func (s *SyslogWriter) Write(p []byte) (int, error) {
+	return s.conn.Write(s.frame(s.format(p)))
 }
 
 type BranchWriter struct {
+	mu     sync.Mutex
 	dsts   []io.Writer
 	buf    []byte
 	ch     chan []byte
@@ -46,8 +86,8 @@ func (b *BranchWriter) forwardWrites() {
 	// slowest destination/sink dictates how slow we write to *all* destinations.  Or do we want to
 	// async/buffer at the destination level?
 	for data := range b.ch {
-		for dst := range b.dsts {
-			_, err := dst.Write(line)
+		for _, dst := range b.dsts {
+			_, err := dst.Write(data)
 			if err != nil {
 				b.errors = append(b.errors, err)
 			}
@@ -58,6 +98,7 @@ func (b *BranchWriter) forwardWrites() {
 func (b *BranchWriter) Close() error {
 	b.Flush()
 	close(b.ch)
+	return nil // TODO: return accumulated errors here?
 }
 
 // Flush causes all buffered data to be written to the underlying destination writer.  This should
@@ -85,8 +126,8 @@ func (b *BranchWriter) Write(p []byte) (int, error) {
 
 func (b *BranchWriter) write() {
 	select {
-	case w.ch <- w.buf:
-		w.buf = w.buf[:0]
+	case b.ch <- b.buf:
+		b.buf = b.buf[:0]
 	default:
 	}
 }
