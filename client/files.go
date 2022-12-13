32a4bd48c04e1bffd04c25bde7c114773c16d095
Ángel Pérez <angel.perez@canonical.com>
2022-12-13 20:42:01 +0100

    Implement RemovePath and pebble rm command (#146)

--- client/files.go
+++ client/files.go
@@ -251,11 +251,6 @@ type makeDirsItem struct {
 	Group       string `json:"group"`
 }
 
-type fileResult struct {
-	Path  string `json:"path"`
-	Error *Error `json:"error,omitempty"`
-}
-
 // MakeDir creates a directory or directory tree.
 func (client *Client) MakeDir(opts *MakeDirOptions) error {
 	var permissions string
@@ -274,6 +269,73 @@ func (client *Client) MakeDir(opts *MakeDirOptions) error {
 			GroupID:     opts.GroupID,
 			Group:       opts.Group,
 		}},
+	}
+
+	var body bytes.Buffer
+	if err := json.NewEncoder(&body).Encode(&payload); err != nil {
+		return fmt.Errorf("cannot encode JSON payload: %w", err)
+	}
+
+	var result []fileResult
+	headers := map[string]string{
+		"Content-Type": "application/json",
+	}
+	if _, err := client.doSync("POST", "/v1/files", nil, headers, &body, &result); err != nil {
+		return err
+	}
+
+	if len(result) != 1 {
+		return fmt.Errorf("expected exactly one result from API, got %d", len(result))
+	}
+	if result[0].Error != nil {
+		return &Error{
+			Kind:    result[0].Error.Kind,
+			Value:   result[0].Error.Value,
+			Message: result[0].Error.Message,
+		}
+	}
+
+	return nil
+}
+
+// RemovePathOptions holds the options for a call to RemovePath.
+type RemovePathOptions struct {
+	// Path is the absolute path to be deleted (required).
+	Path string
+
+	// Recursive, if true, will delete all files and directories contained
+	// within the specified path, recursively. Defaults to false.
+	Recursive bool
+}
+
+type removePathsPayload struct {
+	Action string            `json:"action"`
+	Paths  []removePathsItem `json:"paths"`
+}
+
+type removePathsItem struct {
+	Path      string `json:"path"`
+	Recursive bool   `json:"recursive"`
+}
+
+type fileResult struct {
+	Path  string `json:"path"`
+	Error *Error `json:"error,omitempty"`
+}
+
+// RemovePath deletes a file or directory.
+// The error returned is a *Error if the request went through successfully
+// but there was an OS-level error deleting a file or directory, with the Kind
+// field set to the specific error kind, for example "permission-denied".
+func (client *Client) RemovePath(opts *RemovePathOptions) error {
+	payload := &removePathsPayload{
+		Action: "remove",
+		Paths: []removePathsItem{
+			{
+				Path:      opts.Path,
+				Recursive: opts.Recursive,
+			},
+		},
 	}
 
 	var body bytes.Buffer
