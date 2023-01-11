0a609ea87b71ccdb21e29583200becae5993a538
Fred Lotter <fred.lotter@canonical.com>
2023-01-11 09:12:10 +0200

    Use RandomString() from randutil package
    Make pebble to use a strutil.MakeRandomString() function
    equivalent from the randutil package instead.
    
    This follows the same change was made in Snapd.
    
    This change removes rand.Seed() calls from the strutil package,
    which allows all RNG seeding logic to now live in the randutil
    package, which prevents multiple packages seeding the RNG.

--- internal/osutil/io.go
+++ internal/osutil/io.go
@@ -22,7 +22,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/canonical/x-go/strutil"
+	"github.com/canonical/x-go/randutil"
 
 	"github.com/canonical/pebble/internal/osutil/sys"
 )
@@ -92,7 +92,7 @@ func NewAtomicFile(filename string, perm os.FileMode, flags AtomicWriteFlags, ui
 	// aa-enforce. Tools from this package enumerate all profiles by loading
 	// parsing any file found in /etc/apparmor.d/, skipping only very specific
 	// suffixes, such as the one we selected below.
-	tmp := filename + "." + strutil.MakeRandomString(12) + "~"
+	tmp := filename + "." + randutil.RandomString(12) + "~"
 
 	fd, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_TRUNC|os.O_EXCL, perm)
 	if err != nil {
