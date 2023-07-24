canonical/pebble

commit 4098a71cd2ebe7125d54dd416e62554f3be8cfca

fix(enter): stop pebble if default services cannot start
Exit with non-zero error code if waiting for the "change" (of default
services start) to be ready returns any errors.

internals/cli/cmd_run.go
@@ -194,7 +194,7 @@ func runDaemon(rcmd *cmdRun, ch chan os.Signal, ready chan<- func()) error {
 
 	logger.Debugf("activation done in %v", time.Now().Truncate(time.Millisecond).Sub(t0))
 
-	var autoStartReady chan struct{}
+	var autoStartReady chan error
 	var stop chan struct{}
 
 	notifyReady := func() {
@@ -214,17 +214,14 @@ func runDaemon(rcmd *cmdRun, ch chan os.Signal, ready chan<- func()) error {
 
 		if ready != nil {
 			// wait for the default services to start
-			autoStartReady = make(chan struct{}, 1)
+			autoStartReady = make(chan error, 1)
 			go func() {
 				waitCmd := waitMixin{
 					hideProgress: true,
 				}
 				waitCmd.setClient(rcmd.client)
 				_, err := waitCmd.wait(changeID)
-				if err != nil {
-					logger.Debugf("Error starting default services: %v", err)
-				}
-				autoStartReady <- struct{}{}
+				autoStartReady <- err
 			}()
 		}
 	} else if ready != nil {
@@ -246,7 +243,10 @@ out:
 				d.SetDegradedMode(nil)
 				tic.Stop()
 			}
-		case <-autoStartReady:
+		case chgErr := <-autoStartReady:
+			if chgErr != nil {
+				return fmt.Errorf("error starting default services: %w", chgErr)
+			}
 			notifyReady()
 		case <-stop:
 			break out

commit c01f729c560b6f1e7c829fde1badacbeeaff596a

fix: pebble do not stop for default services start error
This reverts commit 1281c967da6093bb3e2e6567a792f83dd860a079
and reverts the hard-error behaviour introduced in
4098a71cd2ebe7125d54dd416e62554f3be8cfca. Pebble should no longer,
at least not due to this commit, exit with non-zero error code if
waiting for the "change" (of default services start) to be ready
returns any errors.

internals/cli/cmd_run.go
@@ -245,7 +245,7 @@ out:
 			}
 		case chgErr := <-autoStartReady:
 			if chgErr != nil {
-				return fmt.Errorf("error starting default services: %w", chgErr)
+				logger.Noticef("Error starting default services: %v", chgErr)
 			}
 			notifyReady()
 		case <-stop:

internals/cli/cmd_enter_test.go
@@ -269,36 +269,3 @@ func (s *PebbleSuite) TestEnterSubCommandWaits(c *C) {
 
 	c.Check(svcStartTime.Before(subCmdExecTime), Equals, true)
 }
-
-// TestEnterDefaultServiceStartError tests whether default service
-// start errors are reported with non-zero exit codes. Note that,
-// for now this is only tested for
-//
-//	pebble enter --run <subcommand> [args..]
-//
-// Discussions about having this error reporting for “pebble run“
-// and similar scenarios can be found at:
-//   - https://github.com/canonical/pebble/pull/257#discussion_r1259070650
-//   - https://github.com/canonical/pebble/issues/167
-func (s *PebbleSuite) TestEnterDefaultServiceStartError(c *C) {
-	layerTemplate := dumbDedent(`
-		services:
-		  srv:
-		    override: replace
-		    command: unknownBinary123
-		    startup: enabled
-	`)
-	layerPath := filepath.Join(s.pebbleDir, "layers", "001-stat.yaml")
-	writeTemplate(layerPath, layerTemplate, nil)
-
-	cmd := []string{"pebble", "enter", "--run", "stop", "srv1"}
-	restore := fakeArgs(cmd...)
-	defer restore()
-
-	exitCode := cli.PebbleMain()
-	c.Check(exitCode, Equals, 1)
-	stderr := s.Stderr()
-	c.Check(stderr, Matches, "^(?s)cannot run pebble: error starting default services.*")
-	c.Check(stderr, Matches, "(?s).*Start service .* executable file not found in \\$PATH.*")
-	c.Check(s.Stdout(), Equals, "")
-}