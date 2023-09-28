4540a7b711931b6cd78126635cfd521c7f24ee8a
Ben Hoyt <ben.hoyt@canonical.com>
2023-09-29 09:59:16 +1300

    Revert "Bump version number for release v1.4.0"
    This reverts commit 0d6516bacdc8e8cac8fc5d9033a208adfce097b6.

--- cmd/version.go
+++ cmd/version.go
@@ -17,7 +17,7 @@ package cmd
 //go:generate ./mkversion.sh
 
 // Version will be overwritten at build-time via mkversion.sh
-var Version = "v1.4.0"
+var Version = "unknown"
 
 func MockVersion(version string) (restore func()) {
 	old := Version
