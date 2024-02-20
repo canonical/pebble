ff7316b911f6cf4a22eaa93c63f6d9e55d378814
Ben Hoyt <ben.hoyt@canonical.com>
2024-02-20 15:13:12 +1300

    Bump up version number for patch release v1.7.1

--- cmd/version.go
+++ cmd/version.go
@@ -17,7 +17,7 @@ package cmd
 //go:generate ./mkversion.sh
 
 // Version will be overwritten at build-time via mkversion.sh
-var Version = "v1.7.0"
+var Version = "v1.7.1"
 
 func MockVersion(version string) (restore func()) {
 	old := Version
