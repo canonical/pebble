62e34c14f3f50a1a6fc466f982f123731d8ba03f
Ben Hoyt <ben.hoyt@canonical.com>
2024-04-12 10:53:33 +1200

    Per JU073, increase Pebble pruneWait from 24h to 7d
    Also increase abortWait from 7d to 14d

--- internals/overlord/overlord.go
+++ internals/overlord/overlord.go
@@ -43,12 +43,14 @@ import (
 var (
 	ensureInterval = 5 * time.Minute
 	pruneInterval  = 10 * time.Minute
-	pruneWait      = 24 * time.Hour * 1
-	abortWait      = 24 * time.Hour * 7
+
+	// In snapd this is 24h, but that's too short in the context of Pebble.
+	pruneWait = 24 * time.Hour * 7
+
+	// In snapd this is 7d, but also increase that in the context of Pebble.
+	abortWait = 24 * time.Hour * 14
 
 	pruneMaxChanges = 500
-
-	defaultCachedDownloads = 5
 )
 
 var pruneTickerC = func(t *time.Ticker) <-chan time.Time {
