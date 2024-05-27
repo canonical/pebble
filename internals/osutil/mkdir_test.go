edcecdde3dcc0c45b69ece84d1d0b273bae4c74b
Tiexin Guo <guotiexin@gmail.com>
2024-05-27 15:02:37 +0800

    test: mkdir chown test

--- internals/osutil/mkdir_test.go
+++ internals/osutil/mkdir_test.go
@@ -16,11 +16,14 @@ package osutil_test
 
 import (
 	"os"
+	"os/user"
+	"strconv"
 	"syscall"
 
 	"gopkg.in/check.v1"
 
 	"github.com/canonical/pebble/internals/osutil"
+	"github.com/canonical/pebble/internals/osutil/sys"
 )
 
 type mkdacSuite struct{}
@@ -183,4 +186,103 @@ func (mkdacSuite) TestMkdirMakeParentsNoChmod(c *check.C) {
 	info, err = os.Stat(tmpDir + "/foo/bar")
 	c.Assert(err, check.IsNil)
 	c.Assert(info.Mode().Perm(), check.Equals, os.FileMode(0o755))
+}
+
+// See .github/workflows/tests.yml for how to run this test as root.
+func (mkdacSuite) TestMkdirChownAndChmod(c *check.C) {
+	if os.Getuid() != 0 {
+		c.Skip("requires running as root")
+	}
+
+	username := os.Getenv("PEBBLE_TEST_USER")
+	group := os.Getenv("PEBBLE_TEST_GROUP")
+	if username == "" || group == "" {
+		c.Fatalf("must set PEBBLE_TEST_USER and PEBBLE_TEST_GROUP")
+	}
+
+	u, err := user.Lookup(username)
+	c.Assert(err, check.IsNil)
+	g, err := user.LookupGroup(group)
+	c.Assert(err, check.IsNil)
+	uid, err := strconv.Atoi(u.Uid)
+	c.Assert(err, check.IsNil)
+	gid, err := strconv.Atoi(g.Gid)
+	c.Assert(err, check.IsNil)
+
+	tmpDir := c.MkDir()
+
+	err = osutil.Mkdir(
+		tmpDir+"/foo",
+		0o777,
+		&osutil.MkdirOptions{
+			Chmod:   true,
+			Chown:   true,
+			UserID:  sys.UserID(uid),
+			GroupID: sys.GroupID(gid),
+		},
+	)
+	c.Assert(err, check.IsNil)
+	c.Assert(osutil.IsDir(tmpDir+"/foo"), check.Equals, true)
+
+	info, err := os.Stat(tmpDir + "/foo")
+	c.Assert(err, check.IsNil)
+	if stat, ok := info.Sys().(*syscall.Stat_t); ok {
+		c.Assert(int(stat.Uid), check.Equals, uid)
+		c.Assert(int(stat.Gid), check.Equals, gid)
+	}
+	c.Assert(info.Mode().Perm(), check.Equals, os.FileMode(0o777))
+}
+
+// See .github/workflows/tests.yml for how to run this test as root.
+func (mkdacSuite) TestMkdirMakeParentsChownAndChown(c *check.C) {
+	if os.Getuid() != 0 {
+		c.Skip("requires running as root")
+	}
+
+	username := os.Getenv("PEBBLE_TEST_USER")
+	group := os.Getenv("PEBBLE_TEST_GROUP")
+	if username == "" || group == "" {
+		c.Fatalf("must set PEBBLE_TEST_USER and PEBBLE_TEST_GROUP")
+	}
+
+	u, err := user.Lookup(username)
+	c.Assert(err, check.IsNil)
+	g, err := user.LookupGroup(group)
+	c.Assert(err, check.IsNil)
+	uid, err := strconv.Atoi(u.Uid)
+	c.Assert(err, check.IsNil)
+	gid, err := strconv.Atoi(g.Gid)
+	c.Assert(err, check.IsNil)
+	tmpDir := c.MkDir()
+
+	err = osutil.Mkdir(
+		tmpDir+"/foo/bar",
+		0o777,
+		&osutil.MkdirOptions{
+			MakeParents: true,
+			Chmod:       true,
+			Chown:       true,
+			UserID:      sys.UserID(uid),
+			GroupID:     sys.GroupID(gid),
+		},
+	)
+	c.Assert(err, check.IsNil)
+	c.Assert(osutil.IsDir(tmpDir+"/foo"), check.Equals, true)
+	c.Assert(osutil.IsDir(tmpDir+"/foo/bar"), check.Equals, true)
+
+	info, err := os.Stat(tmpDir + "/foo")
+	c.Assert(err, check.IsNil)
+	c.Assert(info.Mode().Perm(), check.Equals, os.FileMode(0o777))
+	if stat, ok := info.Sys().(*syscall.Stat_t); ok {
+		c.Assert(int(stat.Uid), check.Equals, uid)
+		c.Assert(int(stat.Gid), check.Equals, gid)
+	}
+
+	info, err = os.Stat(tmpDir + "/foo/bar")
+	c.Assert(err, check.IsNil)
+	c.Assert(info.Mode().Perm(), check.Equals, os.FileMode(0o777))
+	if stat, ok := info.Sys().(*syscall.Stat_t); ok {
+		c.Assert(int(stat.Uid), check.Equals, uid)
+		c.Assert(int(stat.Gid), check.Equals, gid)
+	}
 }
