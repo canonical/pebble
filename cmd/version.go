dc996b4e0e7699c59de75af0a45d7cdba83c358d
Ben Hoyt <ben.hoyt@canonical.com>
2024-07-08 11:52:52 +1200

    chore: update version to v1.15.0-dev for development

--- cmd/version.go
+++ cmd/version.go
@@ -17,7 +17,7 @@ package cmd
 //go:generate ./mkversion.sh
 
 // Version will be overwritten at build-time via mkversion.sh
-var Version = "v1.14.0"
+var Version = "v1.15.0-dev"
 
 func MockVersion(version string) (restore func()) {
 	old := Version
