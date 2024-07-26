3c97f750759ee2828c9230bda1e62b5e23aa5712
Tiexin Guo <guotiexin@gmail.com>
2024-07-26 15:23:11 +0800

    docs: rename automated doc script and refactor

--- docs/generate_command_docs.py
+++ docs/generate_command_docs.py
@@ -1,3 +1,5 @@
+"""This module generate reference docs for pebble CLI commands."""
+
 import logging
 import os
 import re
@@ -12,64 +14,42 @@ AUTOMATED_START_MARKER = "<!-- START AUTOMATED OUTPUT -->"
 AUTOMATED_STOP_MARKER = "<!-- END AUTOMATED OUTPUT -->"
 
 TEMPLATE = """\
-(reference_pebble_cmd_command)=
-# cmd command
+(reference_pebble_{command}_command)=
+# {command} command
 
-Description
+{description}
 
 ## Usage
 
 <!-- START AUTOMATED OUTPUT -->
-```{terminal}
+```{{terminal}}
    :input: command
 ```
 <!-- END AUTOMATED OUTPUT -->
 """
 
 
-def get_all_pebble_commands() -> typing.List[str]:
-    cmd = "pebble help --all"
-    p = subprocess.run(cmd.split(), text=True, capture_output=True)
-    if p.returncode != 0:
-        logger.error("Error running {}.".format(cmd))
-        exit(1)
-    return re.findall(r"\n\s{4}(\w+)", p.stdout)
+def get_all_commands() -> typing.List[str]:
+    process = subprocess.run(
+        ["go", "run", "../cmd/pebble", "help", "--all"],
+        text=True,
+        capture_output=True,
+        check=True,
+    )
+    return sorted(re.findall(r"\n\s{4}([\w-]+)", process.stdout))
 
 
-def get_command_help_output(cmd: str) -> str:
-    p = subprocess.run(cmd.split(), text=True, capture_output=True)
-    if p.returncode != 0:
-        logger.error("Error running {}.".format(cmd))
-        exit(1)
-    return p.stdout
+def get_command_help_output(cmd: typing.List[str]) -> str:
+    return subprocess.run(cmd, text=True, capture_output=True, check=True).stdout
 
 
 def get_description_from_output(text: str) -> str:
-    pattern = r"Usage:\n.*?\n\n(.*?)\n.*"
+    pattern = r"Usage:\n.*?\n\n(.*?\.)\n.*"
     match = re.search(pattern, text, re.DOTALL)
     if match:
         desired_block = match.group(1).strip()
         return desired_block
     return ""
-
-
-def render_title(text: str, cmd: str) -> str:
-    text = re.sub(
-        r"\(reference_pebble_cmd_command\)=",
-        f"(reference_pebble_{cmd}_command)=",
-        text,
-        flags=re.MULTILINE,
-    )
-    return re.sub(
-        r"^# cmd command$",
-        f"# {cmd} command",
-        text,
-        flags=re.MULTILINE,
-    )
-
-
-def render_description(text: str, description: str) -> str:
-    return re.sub(r"^Description$", f"{description}", text, flags=re.MULTILINE)
 
 
 def render_code_block_cmd(text: str, cmd: str) -> str:
@@ -89,34 +69,26 @@ def update_toc(all_cmds: typing.List[str]):
 
     start_index = text.find("```{toctree}")
     end_index = text.find("```", start_index + 1) + 3
-    cmd_list = "\n".join(["{cmd} <{cmd}>".format(cmd=cmd) for cmd in all_cmds])
-    toc_tree = """\
+    cmd_list = "\n".join(f"{cmd} <{cmd}>" for cmd in all_cmds)
+
+    toc_tree = f"""\
 ```{{toctree}}
 :titlesonly:
 :maxdepth: 1
 
 {cmd_list}
-```""".format(cmd_list=cmd_list)
+```"""
 
     text = text[:start_index] + toc_tree + text[end_index:]
     with open(index_page, "w") as file:
         file.write(text)
 
 
-def git_diff() -> int:
-    cmd = "git diff --exit-code"
-    p = subprocess.run(cmd.split())
-    if p.returncode != 0:
-        logger.warning("Docs are updated, please commit them.")
-    return p.returncode
-
-
 def create_file_if_not_exist(filepath: str, cmd: str) -> bool:
     file_existed = os.path.exists(filepath)
     if not file_existed:
         logger.info(
-            "The doc for command {} doesn't exist, "
-            "creating from the template.".format(cmd)
+            "The doc for command %s doesn't exist, creating from the template.", cmd
         )
         with open(filepath, "w") as file:
             file.write(TEMPLATE)
@@ -124,26 +96,25 @@ def create_file_if_not_exist(filepath: str, cmd: str) -> bool:
 
 
 def generate_help_command_and_output(cmd: str) -> typing.Tuple[str, str]:
-    help_cmd = "pebble {} --help".format(cmd)
+    help_cmd = ["pebble", cmd, "--help"]
+    help_cmd_str = " ".join(help_cmd)
+    help_cmd_output = get_command_help_output(help_cmd).strip()
 
-    help_cmd_output = """\
+    output = f"""\
 <!-- START AUTOMATED OUTPUT -->
 ```{{terminal}}
-:input: {help_cmd}
-{stdout}
+:input: {help_cmd_str}
+{help_cmd_output}
 ```
-<!-- END AUTOMATED OUTPUT -->""".format(
-        help_cmd=help_cmd,
-        stdout=get_command_help_output(help_cmd).strip(),
-    )
+<!-- END AUTOMATED OUTPUT -->"""
 
-    return help_cmd, help_cmd_output
+    return help_cmd, output
 
 
 def process_command(cmd: str):
-    logger.info("Processing doc for command {}".format(cmd))
+    logger.info("Processing doc for command %s.", cmd)
 
-    file_path = "reference/cli-commands/{}.md".format(cmd)
+    file_path = f"reference/cli-commands/{cmd}.md"
     file_existed = create_file_if_not_exist(file_path, cmd)
 
     with open(file_path, "r") as file:
@@ -151,31 +122,33 @@ def process_command(cmd: str):
 
     if AUTOMATED_START_MARKER not in text:
         logger.info(
-            "The marker for automated doc generation is not found in the doc, ignore."
+            'The marker for automated doc generation is not found in the "%s" doc, ignore.',
+            cmd,
         )
         return
 
     help_cmd, help_cmd_output = generate_help_command_and_output(cmd)
-    text = render_code_block_cmd(text, help_cmd)
-    text = render_code_block_output(text, help_cmd_output)
 
     if not file_existed:
-        text = render_title(text, cmd)
-        text = render_description(text, get_description_from_output(help_cmd_output))
+        text = text.format(
+            command=cmd, description=get_description_from_output(help_cmd_output)
+        )
+
+    text = render_code_block_cmd(text, help_cmd)
+    text = render_code_block_output(text, help_cmd_output)
 
     with open(file_path, "w") as file:
         file.write(text)
 
 
 def main():
-    all_cmds = get_all_pebble_commands()
+    all_cmds = get_all_commands()
     for cmd in all_cmds:
         process_command(cmd)
 
     logger.info("Update toc tree.")
     update_toc(all_cmds)
     logger.info("Done!")
-    exit(git_diff())
 
 
 if __name__ == "__main__":
