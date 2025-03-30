987a08d918907011d6223eb289e3c39d1a318a3d
Ben Hoyt <ben.hoyt@canonical.com>
2025-03-31 12:07:32 +1300

    chore: bump version to v1.21.0-dev for development window

--- cmd/version.go
+++ cmd/version.go
@@ -17,7 +17,7 @@ package cmd
 //go:generate ./mkversion.sh
 
 // Version will be overwritten at build-time via mkversion.sh
-var Version = "v1.20.0"
+var Version = "v1.21.0-dev"
 
 func MockVersion(version string) (restore func()) {
 	old := Version
