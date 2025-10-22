9d51c7deb4037e085c26c44271f4a47e51a4c5e3
Frederik Lotter <fred.lotter@canonical.com>
2025-10-22 20:53:47 +0200

    feat(client): mTLS support [2b/2] (#715)
    The client side mTLS code was split from #708.
    
    For the complete description of the work, please refer to the
    description in #708.
    
    Pebble client changes adds:
    - The ability to supply the client identity certificate, but this is not
    yet used by the CLI and therefore Pebble cannot use HTTPS today.
    - The ability to obtain the server identity certificate by making an
    insecure HTTPS connection. Can be used to implement an SSH-like workflow
    to implement a CLI warning asking the user to trust the server identity
    for future connections.
    - The ability to pair with the server by supplying the server identity
    fingerprint. This workflow requires obtaining the fingerprint by other
    means, such as DNSSD or via a display on the server side.

---
+++ client/pairing.go
@@ -0,0 +1,50 @@
+// Copyright (c) 2025 Canonical Ltd
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package client
+
+import (
+	"bytes"
+	"context"
+	"crypto/x509"
+	"encoding/json"
+	"fmt"
+)
+
+type pairingPayload struct {
+	Action string `json:"action"`
+}
+
+// Pair pairs the client with the Pebble server using mTLS authentication.
+// This establishes a trusted relationship between the client and server
+// while the server has pairing mode enabled.
+func (client *Client) Pair() (idCert *x509.Certificate, err error) {
+	payload := pairingPayload{
+		Action: "pair",
+	}
+	body, err := json.Marshal(&payload)
+	if err != nil {
+		return nil, fmt.Errorf("cannot marshal pairing payload: %w", err)
+	}
+	resp, err := client.Requester().Do(context.Background(), &RequestOptions{
+		Type:   SyncRequest,
+		Method: "POST",
+		Path:   "/v1/pairing",
+		Body:   bytes.NewReader(body),
+	})
+	if err == nil && resp != nil {
+		idCert = resp.TLSServerIDCert
+	}
+	return idCert, err
+}
