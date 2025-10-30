ece1ce6934e1417c0bed3385dd297d5d0de8663f
David Wilding <david.wilding@canonical.com>
2025-10-31 07:25:10 +0800

    update outdated comment in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -240,19 +240,6 @@ linkcheck_retries = 3
 # https://www.sphinx-doc.org/en/master/usage/extensions/index.html
 
 # NOTE: The canonical_sphinx extension is required for the starter pack.
-#       It automatically enables the following extensions:
-#       - custom-rst-roles
-#       - myst_parser
-#       - notfound.extension
-#       - related-links
-#       - sphinx_copybutton
-#       - sphinx_design
-#       - sphinx_reredirects
-#       - sphinx_tabs.tabs
-#       - sphinxcontrib.jquery
-#       - sphinxext.opengraph
-#       - terminal-output
-#       - youtube-links
 
 extensions = [
     "canonical_sphinx",
