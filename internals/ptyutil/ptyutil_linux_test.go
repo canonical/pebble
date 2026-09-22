91a344d43f57257f815ef148838b784e29816a45
Chapman von Monty <chapman.von.monty@fastmail.com>
2026-09-23 09:59:38 +1000

    fix(ptyutil): handle Openat error and close peer fd on failure in OpenPtyInDevpts (#1087)
    `OpenPtyInDevpts` could panic when opening `ptmx` through an invalid
    `devpts` descriptor, and could leak the peer descriptor when `TIOCGPTN`
    failed.
    
    This returns `Openat` errors immediately instead of dereferencing a
    `nil` master file, and close peer descriptors on post-`TIOCGPTPEER`
    failures.
    
    ## Technical Details / Changes
    
    - Return the unix.Openat` error before installing cleanup or issuing
    `ioctl`s.
    - Close `ptyFd` when `TIOCGPTN` fails after `TIOCGPTPEER` succeeds.
    - Add regression coverage for invalid `devpts` descriptors.
    
    ## QA / Verification
    
    - `go test ./internals/ptyutil/...`
    - `go test ./...`
    
    ## Links
    
    Fixes #1029
    Fixes #1030

---
+++ internals/ptyutil/ptyutil_linux_test.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2026 Canonical Ltd
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package ptyutil
+
+import (
+	"os"
+
+	. "gopkg.in/check.v1"
+)
+
+type ptyutilSuite struct{}
+
+var _ = Suite(&ptyutilSuite{})
+
+func (s *ptyutilSuite) TestOpenPtyInDevptsInvalidDevptsFd(c *C) {
+	f, err := os.Open("/dev/null")
+	c.Assert(err, IsNil)
+	fd := int(f.Fd())
+	c.Assert(f.Close(), IsNil)
+
+	ptx, pty, err := OpenPtyInDevpts(fd, int64(os.Getuid()), int64(os.Getgid()))
+	c.Check(ptx, IsNil)
+	c.Check(pty, IsNil)
+	c.Check(err, NotNil)
+}
